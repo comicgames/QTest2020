cfd77f3950b47743dac97d443f1f3e43d4036177
Jared School <jaredmartinez52101@gmail.com>
2020-03-02 22:44:29 -0800

    light work
    I had to change the target mode to windows cuz im on windows so ya change it back when u on mac
    
    also worked on amazing movement script a lil

---
+++ Assets/Scripts/Amazing Movement Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmazingMovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        float pm_stopspeed = 100.0f;
+        float pm_duckScale = 0.25f;
+        float pm_swimScale = 0.50f;
+        float pm_wadeScale = 0.70f;
+
+        float pm_accelerate = 10.0f;
+        float pm_airaccelerate = 1.0f;
+        float pm_wateraccelerate = 4.0f;
+        float pm_flyaccelerate = 8.0f;
+
+        float pm_friction = 6.0f;
+        float pm_waterfriction = 1.0f;
+        float pm_flightfriction = 3.0f;
+        float pm_spectatorfriction = 5.0f;
+        int c_pmove = 0;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
